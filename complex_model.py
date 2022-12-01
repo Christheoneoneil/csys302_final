093a386532d57eb501ee2e202fa692fb74e69dc7
Christheoneoneil <78617902+Christheoneoneil@users.noreply.github.com>
2022-12-01 23:15:06 +0000

    implemented semi-workimg good driver only

--- complex_model.py
+++ complex_model.py
@@ -19,7 +19,7 @@ def generate_drivers(num_drivers: int, bad_driver_prop: float, states: list) -> 
 
         """
 
-        drivers = {key: random.choices(states, [1-bad_driver_prop, bad_driver_prop]) for key in np.arange(1,num_drivers+1, 1)}
+        drivers = [{key: random.choices(states, [1-bad_driver_prop, bad_driver_prop])[0]} for key in np.arange(1,num_drivers+1, 1)]
         return drivers
 
 
@@ -34,10 +34,32 @@ def run_model(drivers: list, network: nx.Graph(), origin_node: int, end_node: in
         number of iterations required for all drivers to get to final
         destination
         """
-        network.nodes[origin_node]["Queue"].append(drivers)
-        good_driver_path = nx.shortest_path(network, source=origin_node, target=end_node, method='dijkstra', weight="length")
-        
 
-driver_list = generate_drivers(1000, .5, ["good", "bad"])
+
+        network.nodes[origin_node]["Queue"] +=  drivers
+        good_driver_path = nx.shortest_path(network, source=origin_node, target=end_node, method='dijkstra', weight="length")
+        init_drivers = [list(driver.keys())[0] for driver in network.nodes[origin_node]["Queue"]]
+        end_drivers = []
+        active_nodes = [origin_node]
+        
+        while set(init_drivers) != end_drivers:
+                try:       
+                        for node in active_nodes.copy():
+                                first_out = network.nodes[node]["Queue"].pop(0)
+                                state = list(first_out.values())[0]
+                                if state == "good":
+                                        ind = good_driver_path.index(node)
+                                        next_step = good_driver_path[ind+1]
+                                        network.nodes[next_step]["Queue"].append(first_out)
+                                active_nodes.append(next_step)
+                        try:
+                                end_drivers = set([list(driver.keys())[0] for driver in network.nodes[end_drivers]["Queue"]])
+
+                        except TypeError:
+                                continue
+                except IndexError:
+                        continue
+                
+driver_list = generate_drivers(1000, 0, ["good", "bad"])
 net = gen_net(gen_data(), ["u", "v", "length"])
 run_model(driver_list, net, 204449959, 204350837)
