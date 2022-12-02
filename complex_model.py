af370e143b00530bd2faef0384def501aba0682a
Christheoneoneil <78617902+Christheoneoneil@users.noreply.github.com>
2022-12-02 15:57:00 +0000

    refactored while loop for bad drivers

--- complex_model.py
+++ complex_model.py
@@ -1,7 +1,7 @@
 import random 
+import copy
 import networkx as nx
 import numpy as np
-import copy
 from gen_complex_net import gen_net
 from gen_complex_net import gen_data
 
@@ -56,26 +56,27 @@ def run_model(drivers: list, network: nx.Graph(), origin_node: int, end_node: in
         a flag was not created to just stop popping off end_drivers node, so once the last
         driver is in the end_drivers queue the loop ends and we return a count
         """
-        while len(end_drivers) == 0 or sorted(init_drivers)[-1] != end_drivers[-1]:
+        while len(end_drivers) == 0 or sorted(init_drivers)[-1] != sorted(end_drivers)[-1]:
                 iterations += 1
-                print(iterations)
-                print(active_nodes)
-                print(end_drivers)
                 # Keep this as deepcopy or for loop behavior changes
                 for node in copy.deepcopy(active_nodes):
-                        try:        
-                                drivers = network.nodes[node]["Queue"]
-                                end_drivers = [list(driver.keys())[0] for driver in network.nodes[end_node]["Queue"]]
-                                first_out = drivers.pop(0)
-                                state = list(first_out.values())[0]
-                                if state == "good":
-                                        current_node_ind = good_driver_path.index(node)
-                                        next_step = good_driver_path[current_node_ind + 1]
-                                        network.nodes[next_step]["Queue"].append(first_out)
-                        except IndexError:
-                                # Once all drivers are dequed remove node from active
-                                # nodes list
-                                active_nodes.remove(node)
+                        if node != end_node:
+                                try:        
+                                        drivers = network.nodes[node]["Queue"]
+                                        
+                                        first_out = drivers.pop(0)
+                                        state = list(first_out.values())[0]
+                                        if state == "good":
+                                                current_node_ind = good_driver_path.index(node)
+                                                next_step = good_driver_path[current_node_ind + 1]
+                                                network.nodes[next_step]["Queue"].append(first_out)
+                                        else:
+                                                pass
+                                except IndexError:
+                                        # Once all drivers are dequed remove node from active
+                                        # nodes list
+                                        active_nodes.remove(node)
+                end_drivers = [list(driver.keys())[0] for driver in network.nodes[end_node]["Queue"]]
                 active_nodes.add(next_step)
         
         return iterations
@@ -83,4 +84,5 @@ def run_model(drivers: list, network: nx.Graph(), origin_node: int, end_node: in
 
 driver_list = generate_drivers(1000, 0, ["good", "bad"])
 net = gen_net(data=gen_data(), node_vals=["u", "v", "length"])
-run_model(driver_list, net, 204449959, 204350837)
+# origin and end node were found by our jupyiter notebook
+print(run_model(drivers=driver_list, network=net, origin_node=204449959, end_node=204350837))
